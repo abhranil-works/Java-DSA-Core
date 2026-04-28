ba2f210785bdbab0c87f1e4688a2811fec0e57cd
abhranil.works <abhranil.works@gmail.com>
2026-04-28 20:57:07 +0530

    Added Inverted Half Pyramid Pattern

---
+++ InvertedHalfPyramid.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+  public class InvertedHalfPyramid {
+
+
+    public static void printPattern(int rowCount, String symbol) {
+
+        //Using Nested 'for-loop' for the pattern logic.
+
+        if (rowCount > 0) { //Using 'if-else' to protect from negative values', which can instantly terminat the loop.
+        //Outer loop.
+        for(int i = rowCount; i >= 1; i--) {
+
+            // Inner loop.
+            for(int j = 1; j <= i; j++) {
+                System.out.print(symbol + " "); //It will make some space for better visibility. 
+
+            }
+
+            System.out.println(); //Spaces as rows for inner symbol printing.
+        }
+
+    } else {
+
+        System.out.println(" Your Inputs (Row and Column) do not match our condition. Please enter values greater than 0.");
+    }
+
+
+    }
+
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Let's Print a Inverted Half Pyramid Pattern.");
+        System.out.print("\n Enter The Row Number (Without Decimal): ");
+        int rowCount = sc.nextInt();
+        
+
+        sc.nextLine(); //Using it as a 'Buffer' for cleaning the 'Enter or Return' key (that is a String) to protect the 'symbol input' space.
+
+        System.out.print("\n Enter the symbol (e.g. @, #, *, A,a etc.) you want to show in you pattern: ");
+        String symbol = sc.nextLine();
+
+        System.out.println(" Result:____________\n");
+
+        printPattern(rowCount,symbol);
+        
+
+        sc.close(); //Closing the 'Scanner' object to prevent memory leaks.
+          
+      }
+  }
